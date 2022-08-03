ab26aeac36ae63320d95e2286ef549886df77f4f
Parthasarathy <parthasarathi.parandhaman@gmail.com>
2022-08-03 16:29:48 +0530

    added a subraction

--- add.sh
+++ add.sh
@@ -6,5 +6,8 @@ echo "Enter value two: "
 read b
 
 sum=`expr $a + $b `
+sub=`expr $a - $b `
 
-echo "Total value of two number is: $sum"
+echo "Total value of two number is: "$sum
+
+echo "subraction of two number is: "$sub
